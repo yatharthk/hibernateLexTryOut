d6876ddfa3f3ec608ab3b0f483ea30a24745e3de
yatharthk <82939639+yatharthk@users.noreply.github.com>
2022-10-31 11:37:44 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,48 +1,37 @@
 package org.example;
 
-import org.example.entity.Customer;
-import org.example.repository.CustomerRepository;
-import org.example.repository.CustomerRepositoryImpl;
-import org.example.service.CustomerService;
-import org.example.service.CustomerServiceImpl;
-import org.example.util.HibernateUtil;
-import org.hibernate.Session;
+import org.myBank.dao.CustomerDaoImpl;
+import org.myBank.dao.MCustomerDao;
+import org.myBank.entity.Customer;
 
-import java.time.LocalDate;
-import java.util.Calendar;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class Main {
+   static Logger LOG = Logger.getLogger(Main.class.getName());
     public static void main(String[] args) {
-
-        System.out.println("Hello world!");
-
-        //new state
-//        Customer customer = new Customer();
-//        customer.setId(1004);
-//        customer.setCustomerName("yk1");
-//        customer.setDateOfBirth(LocalDate.of(1996,03,04));
-
-        System.out.println("Adding a customer");
-
-        CustomerRepository customerRepository = new CustomerRepositoryImpl();
-        CustomerService customerService = new CustomerServiceImpl(customerRepository);
-
-//        customerService.addCustomer(customer);
-//        System.out.println("customer added with id "+customer.getId());
-        System.out.println("Customer details are: ");
-        System.out.println(customerService.getCustomer(1001));
-        System.out.println(customerService.getCustomer(1002));
-        System.out.println(customerService.getCustomer(1003));
-
-        Customer customer2 = new Customer();
-        customer2.setCustomerName("skundra");
-        customer2.setDateOfBirth(LocalDate.of(1996,01,14));
-        customerService.updateCustomer(1002,customer2);
-
-        customerService.deleteCustomer(1004);
-
-
-
+        MCustomerDao customerDao = new CustomerDaoImpl();
+        try {
+            LOG.info("Enter the Customer details");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the Customer name");
+            String name = sc.next();
+            System.out.println("Enter the Customer date of birth in MM/dd/yyyy format");
+            String dateStr = sc.next();
+            SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+            Date dob = formatter.parse(dateStr);
+            System.out.println("Enter the Customer address");
+            String address = sc.next();
+            System.out.println("Enter the Customer phoneNo");
+            Long phone = sc.nextLong();
+            Customer customer = new Customer(name, dob, address, phone);
+            customerDao.addCustomer(customer);
+            LOG.info("One record created");
+            sc.close();
+        } catch (Exception e) {
+            LOG.severe(e.getMessage());
+        }
     }
 }
